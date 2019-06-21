9e4bab0db0e91f059c9294cbcb64826f80074600
russel-tsang <russel.tsang@gmail.com>
2019-06-21 17:28:41 -0400

    Initial upload - linked list

--- HashMap&LRUCache/lib/p04_linked_list.rb
+++ HashMap&LRUCache/lib/p04_linked_list.rb
@@ -1,3 +1,4 @@
+require "byebug"
 class Node
   attr_reader :key
   attr_accessor :val, :next, :prev
@@ -20,7 +21,14 @@ class Node
 end
 
 class LinkedList
+  attr_reader :head, :tail
+
+  include Enumerable
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head 
   end
 
   def [](i)
@@ -29,30 +37,76 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    self.head.next == self.tail && self.tail.prev == self.head
+    
   end
 
   def get(key)
+    current_node = @head
+    until current_node == nil
+      if current_node.key == key
+        return current_node.val
+      else
+        current_node = current_node.next
+      end
+    end
   end
 
   def include?(key)
+    current_node = @head
+    until current_node == nil
+      if current_node.key == key
+        return true
+      end
+     current_node = current_node.next
+    end
+    false
   end
 
   def append(key, val)
+    new_node = Node.new(key, val)
+    temp_node = @tail.prev
+    @tail.prev = new_node
+    temp_node.next = new_node
+    new_node.prev = temp_node
+    new_node.next = @tail
   end
 
   def update(key, val)
+    current_node = @head
+    until current_node == nil
+      if current_node.key == key
+       current_node.val = val
+      end
+     current_node = current_node.next
+    end
   end
 
   def remove(key)
+    current_node = @head
+    until current_node == nil
+      if current_node.key == key
+       current_node.next.prev = current_node.prev 
+       current_node.prev.next = current_node.next
+      end
+     current_node = current_node.next
+    end
   end
 
-  def each
+  def each(&prc)
+    current_node = self.head
+    until current_node == nil
+      prc.call(current_node) unless current_node.val == nil
+     current_node = current_node.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
