6f173e7428f564ab40e062a32974f54ffefb0ee8
russel-tsang <russel.tsang@gmail.com>
2019-06-21 17:26:44 -0400

    Initial upload - hashing functions

--- HashMap&LRUCache/lib/p02_hashing.rb
+++ HashMap&LRUCache/lib/p02_hashing.rb
@@ -4,11 +4,17 @@ end
 
 class Array
   def hash
+    h = ""
+    self.each do |i|
+      h += (i * 2).to_s
+    end
+    h.to_i 
   end
 end
 
 class String
   def hash
+    self.split("").map{|char| char.ord}.join("").to_i
   end
 end
 
@@ -16,6 +22,12 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    # return 0 if nil?
+    h = 0
+    self.to_a.flatten.each do |ele|
+      h += ele.hash
+    end
+    return h
+    # 0
   end
 end
